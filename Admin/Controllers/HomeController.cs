48206a2966bc121485ab42118ea0cad26aec5640
Gilberto Junior <gilbertojunior533@gmail.com>
2018-12-07 10:15:17 -0200

    Adicionado método de relatóriospara o dashboard.

--- Admin/Controllers/HomeController.cs
+++ Admin/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Admin.Helppers;
+using Admin.Helppser;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -25,6 +28,32 @@ namespace Admin.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult DashTotaisHome()
+        {
+            var usuario = PixCoreValues.UsuarioLogado;
+            var keyUrl = ConfigurationManager.AppSettings["UrlAPI"].ToString();
+            var url = keyUrl + "/Seguranca/WpRelatorios/Totais/" + usuario.idCliente + "/" +
+                PixCoreValues.UsuarioLogado.IdUsuario;
+
+            var helper = new ServiceHelper();
+            var result = helper.Get<IEnumerable<object>>(url);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult SaldoEmpresas()
+        {
+            var usuario = PixCoreValues.UsuarioLogado;
+            var keyUrl = ConfigurationManager.AppSettings["UrlAPI"].ToString();
+            var url = keyUrl + "/Seguranca/WpRelatorios/SaldoEmpresas/" + usuario.idCliente + "/" +
+                PixCoreValues.UsuarioLogado.IdUsuario;
+
+            var helper = new ServiceHelper();
+            var result = helper.Get<IEnumerable<object>>(url);
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
     }
 }
